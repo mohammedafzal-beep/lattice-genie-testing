7ce9e575a83adb461195bca5afed1f9e7d78cab9
mohammedafzal-beep <mo0002al@e.ntu.edu.sg>
2025-12-14 21:45:34 +0400

    Refactor right_column for STL handling and downloads
    Refactor right_column function to simplify STL file handling and improve download button functionality.

--- columns/right_col.py
+++ columns/right_col.py
@@ -1,42 +1,17 @@
 import streamlit as st
 from streamlit_stl import stl_from_file
 from utils.dataloader import log_event, log_submission
-import time
-def right_column():
+from components.parameter_ui import labeled_slider
+def right_column(data):
     st.markdown('<h3 style="display: flex; align-items: center; justify-content: center; margin-top: -20px; text-align:center;">📸 Preview </h3>', unsafe_allow_html=True)
-
+    
     if st.session_state['stl_path']:
-        if "current_index" not in st.session_state:
-            st.session_state.current_index = len(st.session_state['stl_files'])-1
-        with col1:
-            if st.session_state.current_index > 0:
-                if st.button("←"):
-                    st.session_state.current_index -= 1
-                    st.session_state['switch'] = True
-
-        with col3:
-            if st.session_state.current_index < len(st.session_state['stl_files'])-1:
-                if st.button("→"):
-                    st.session_state.current_index += 1
-                    st.session_state['switch'] = True
-        with col2:
-            path = st.session_state['stl_files'].keys()[st.session_state.current_index]
-            if st.session_state['stl_files'] != {}:
-                stl_from_file(path,st.session_state.get('stl_color', '#336fff'), 
-                        auto_rotate=True, width=700, height=500,
-                        cam_distance=100*(current_params['resolution']/50),cam_h_angle=45,cam_v_angle=75)
-                schema = data["params_dict"].get(st.session_state['dict_key'], 1)
-                st.session_state['slider_placeholder'].empty()
-                with st.session_state['slider_placeholder']:
-                    for param_key in schema:
-                        labeled_slider(param_key,schema[param_key], st.session_state['current_params'],
-                        preset=True)
-         
-            else:
-              stl_from_file(st.session_state['stl_path'],st.session_state.get('stl_color', '#336fff'), 
-                        auto_rotate=True, width=700, height=500,
-                        cam_distance=100*(current_params['resolution']/50),cam_h_angle=45,cam_v_angle=75)
-              
+        
+        current_params = st.session_state['current_params']
+        stl_from_file(st.session_state['stl_path'],st.session_state.get('stl_color', '#336fff'), 
+                auto_rotate=True, height=310,
+                cam_distance=84*(current_params['resolution']/50),cam_h_angle=45,cam_v_angle=75)
+        
             
         # Download button
         try:
@@ -45,16 +20,18 @@ def right_column():
             with design_ques_tab[1]:
                 design_ques = st.selectbox("Design Question", design_ques_list, index=0)
                 log_event(design_ques,'Chat mode')
-            download_submit_tab = st.columns([1, 1, 1])
-            with download_submit_tab[2]:
-                if st.button('Submit'):
-                    log_submission(st.session_state['confirmed_params'],design_ques, 'Pro Mode')
-                    st.rerun() 
-            with open(stl_path, 'rb') as f:
+            download_submit_tab = st.columns([1, 1])
+            
+            with open(st.session_state['stl_path'], 'rb') as f:
                 
                 with download_submit_tab[0]:
-                    st.download_button('⬇️ Download STL', data=f.read(), file_name=stl_path, mime='model/stl')
+                    st.download_button('⬇️ Download STL', data=f.read(), file_name=st.session_state['stl_path'], mime='model/stl')
                     log_event('Download','Pro mode')
+            
+            with download_submit_tab[1]:
+                if st.button('Submit'):
+                    log_submission(st.session_state['confirmed_params'],design_ques, 'Pro Mode')
+                    
         except Exception:
             st.warning('STL file not available for download. Generate again.')
     else:
